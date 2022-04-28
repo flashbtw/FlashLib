86ed501e7a3f3f71617304cbc56bcc0523657689
flashbtw <johannes-kurt.reiter@freenet.de>
2022-04-28 12:42:59 +0200

    init commit

---
+++ src/main/java/de/flashyboi/minecraft/plugins/flashlib/FlashLib.java
@@ -0,0 +1,17 @@
+package de.flashyboi.minecraft.plugins.flashlib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FlashLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
